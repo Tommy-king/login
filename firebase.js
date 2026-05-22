ce0096a519c6edb72c9d2661b4ce6c2efeee28f4
Tommy-king <rentalaanil1@outlook.com>
2026-05-22 14:39:33 +0530

    Update and rename firebase-config.js to firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import {
+  getDatabase, ref, set, get, child, update, remove, onValue
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDvBg3NjojMJyfwS2ekhe6KnXjcgmV5qGQ",
     authDomain: "anil-rentala.firebaseapp.com",
@@ -8,4 +13,9 @@ const firebaseConfig = {
     appId: "1:94163866842:web:12caea0e85572c37e341e3",
     measurementId: "G-YXJ6HLSVBE"
   };
-window.firebaseConfig = firebaseConfig;
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export {
+  db, ref, set, get, child, update, remove, onValue
+};
